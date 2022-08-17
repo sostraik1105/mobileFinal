ce267ce75e729a889f1f443910c8ee72f0d7ffe7
Dybuc23 <victorj.2399@gmail.com>
2022-08-16 21:08:43 -0500

    --victor2

--- app/src/main/java/com/example/finalexam/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/finalexam/ui/login/LoginFragment.kt
@@ -1,60 +1,120 @@
 package com.example.finalexam.ui.login
 
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.finalexam.R
+import com.example.finalexam.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentLoginBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        val loginViewModel =
+            ViewModelProvider(this)[LoginViewModel::class.java]
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        val editTextEmail = binding.editTextEmail
+        val editTextPassword = binding.editTextPassword
+        val textView: TextView = binding.textLogin
+
+        // firebase auth instance
+        /*binding.login.setOnClickListener(View.OnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(context, "Please enter email/password", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            } else {
+                signIn(email, password)
+                Navigation.findNavController(root).navigate(R.id.navigation_profile)
+            }
+
+        })*/
+        binding.recover.setOnClickListener(View.OnClickListener {
+            Navigation.findNavController(root).navigate(R.id.navigation_recover)
+
+        })
+
+
+
+        loginViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+/*
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun signIn(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(context, "Authentication success.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.e("Error", task.exception.toString())
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        getUser()
+    }
+
+    private fun signOut() {
+        firebaseAuth.signOut()
+    }
+
+    private fun signUp(email: String, password: String) {
+        val user = hashMapOf(
+            "email" to email,
+            "password" to password
+        )
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(context, "Register success.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.e("Error", task.exception.toString())
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(context, "Register failed.", Toast.LENGTH_SHORT).show()
                 }
             }
     }
+
+    private fun getUser(){
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("User", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("User", "Error getting documents.", exception)
+            }
+    }
+*/
 }
---
+++ app/src/main/java/com/example/finalexam/ui/recover/RecoverPassViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.finalexam.ui.recover
+
+import androidx.lifecycle.ViewModel
+
+class RecoverPassViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
