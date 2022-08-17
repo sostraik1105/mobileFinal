5be63a53b0df910b73aece8fa0a6778a02f03f64
yoelexe <karmahucents@gmail.com>
2022-08-17 08:55:30 -0500

    asd

--- app/src/main/java/com/example/finalexam/ui/StorageActivity.kt
+++ app/src/main/java/com/example/finalexam/ui/StorageActivity.kt
@@ -2,6 +2,7 @@ package com.example.finalexam.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finalexam.R
 
 class StorageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
